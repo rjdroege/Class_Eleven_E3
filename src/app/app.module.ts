dba2047edbcdd694406af9819b643197f71587d6
rjdroege <99696219+rjdroege@users.noreply.github.com>
2022-03-22 16:10:57 -0500

    Exercise Three

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { paragraphList } from './paragraph-list/paragraph-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    paragraphList
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/paragraph-list/paragraph-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'paragraph-list',
+  templateUrl: './paragraph-list.component.html',
+  styleUrls: ['./paragraph-list.component.css']
+})
+
+export class paragraphList{
+
+}
